8b216636b1e18ea1510b74d645ab29be901eb220
warlock <h.kwiatkowski@hotmail.com>
2021-12-22 10:13:27 +0100

    break

---
+++ frontend/src/CreateRoomPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button"
+
+export default class CreateRoomPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the room join page</p>
+    }
+}
+
